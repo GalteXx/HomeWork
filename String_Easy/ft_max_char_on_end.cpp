2fe9fae6a4e8010a1672bdf042d08924e3d22895
GalteXx <65533265+GalteXx@users.noreply.github.com>
2020-11-10 21:20:42 +0300

    Update ft_max_char_on_end.cpp

--- String_Easy/ft_max_char_on_end.cpp
+++ String_Easy/ft_max_char_on_end.cpp
@@ -8,12 +8,13 @@ int ft_max_char_on_end(string str)
     bool b = true;
     int kol1, kol2 = 0, nach, i;
     kol1 = ft_len(str);
+    int z = 0;
     for (i = 0; i < kol1; i++)
     {
         if (str[i] == str[i - 1])
         {
             nach = i - 1;
-            for (int z = 0; z < kol2 && b; z++)
+            for (z = 0; z < kol2 && b; z++)
             {
                 if (str[z] == str[nach])
                 {
