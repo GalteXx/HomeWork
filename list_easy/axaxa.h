9a5667c76f20f4ed27afb393999b73b259608a75
GalteXx <65533265+GalteXx@users.noreply.github.com>
2020-11-26 10:56:25 +0300

    Add files via upload

---
+++ list_easy/axaxa.h
@@ -0,0 +1,14 @@
+#ifndef AXAXA_H_INCLUDED
+#define AXAXA_H_INCLUDED
+
+void ft_super_shift_list(vector <int> &mass, int n);
+void ft_even_index_list(const vector <int> mass, vector <int> &mass2);
+void ft_even_parts_list(const vector <int> mass, vector <int> &mass2);
+int ft_positive_list(const vector <int> mass);
+void ft_rev_list(vector <int> &mass);
+void ft_rev_par_list(vector <int> &mass);
+void ft_rshift_list(vector <int> &mass);
+bool ft_same_parts_list(const vector <int> mass);
+int ft_sl_list(const vector <int> mass);
+
+#endif // AXAXA_H_INCLUDED
