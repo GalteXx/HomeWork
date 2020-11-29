e48df679ff165dbed059ae1b43a2de53bc2f345e
GalteXx <65533265+GalteXx@users.noreply.github.com>
2020-11-29 15:33:59 +0300

    Add files via upload

--- list_easy/ft_super_shift_list.cpp
+++ list_easy/ft_super_shift_list.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<vector>
-//#include "HQ.h"
+#include "HQ.h"
 using namespace std;
 int abs(int a)
 {
@@ -43,24 +43,4 @@ if(n < 0)
 }
 
 
-int main()
-{
-    vector <int> arr;
-    int kol;
-    cin >> kol;
-    for(int i = 0; i < kol; i++)
-    {
-        int a;
-        cin >> a;
-        arr.push_back(a);
-    }
-    int n;
-    cin >> n;
-    ft_super_shift_list(arr, n);
-    for(int i = 0; i < arr.size(); i++)
-    {
-        cout << arr[i];
-    }
-    return 0;
-}
 
