ff399573719744abea05cdd914b043ace397c2d5
GalteXx <65533265+GalteXx@users.noreply.github.com>
2021-03-09 20:08:57 +0300

    oshihiteo

--- Graph/1.cpp
+++ Graph/1.cpp
@@ -2,7 +2,8 @@
 int main()
 {
 initwindow(400,400);
-moveto(0,0);         
+moveto(0,0);
+lineto(0,50);
 getch();             
 closegraph();        
 return 0;
---
+++ Graph/2.cpp
@@ -0,0 +1,10 @@
+#include <graphics.h>
+int main()
+{
+initwindow(400,400);
+moveto(0,0);
+lineto(50, 0);
+getch();             
+closegraph();        
+return 0;
+}
