c6214db967cd377070d7a57915b9bdb9ff5d82a4
GalteXx <65533265+GalteXx@users.noreply.github.com>
2021-04-08 10:43:46 +0300

    Add files via upload

---
+++ stek/rekursionniyBred.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+bool exit = false;
+
+void execute(string str)
+{
+    //if()
+}
+
+void push(vector <int> &vec,int ch)
+{
+    vec.push_back(ch);
+}
+
+void pop(vector <int> &vec)
+{
+    vec.pop_back();
+}
+void backo(const vector <int> &vec)
+{
+    cout << vec[vec.size() - 1];
+}
+void sizez(const vector <int> &vec)
+{
+    cout << vec.size();
+}
+
+void clearr(vector <int> &vec)
+{
+    for(int i = 0; i < vec.size(); i++)
+        vec.pop_back();
+
+    cout << "ok";
+}
+void exitt()
+{
+    exit = true;
+}
+
+int main()
+{
+    vector <int> vec;
+/*
+    while(!exit)
+    {
+        execute();
+    }
+*/
+    string all;
+    cin >>
+    kruke()
+    return 0;
+}
