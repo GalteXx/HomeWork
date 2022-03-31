0aa1ab54078b8f8411c397f3b14fe32b2af2488b
GalteXx <65533265+GalteXx@users.noreply.github.com>
2022-03-31 19:01:02 +0300

    boop

--- 25Ege/net3e.cpp
+++ 25Ege/net3e.cpp
@@ -1,31 +1,10 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
 int main(){
-    int kex;
-    for(int i = 525784203; i <= 728943762; i++){
-        int kol = 0, l;
-        if(sqrt(i) == (int)sqrt(i)){
-            for(int  j = 2; j <= sqrt(i); j++){
-                if(i % j == 0){
-                    if(j == sqrt(i))
-                        kol++;
-                    else{
-                        kol += 2;
-                        l = j;
-                    
-                    }
-                        
-                }
-                if(kol > 3)
-                    break;
-            }
-        if(kol == 3)
-            cout << i << " " << i / l << endl;
-        
-    }
-
-    }
+    int a;
+    cin >> a;
+    cout << a;
     return 0;
 }
--- 5EGE/4.py
+++
@@ -1,4 +0,0 @@
-kol = 0
-for i in range(800, 901):
-    ch = bin(i)[2:]
-    
